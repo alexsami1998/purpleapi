892182f1e16fac9adeba5c2223797a6d8b31afb6
alexsami1998 <alexandre-enter86@live.com>
2023-10-10 16:41:44 -0300

    first commit of the api.

---
+++ purpleapi/src/main/java/br/purpleapi/purple/PurpleapiApplication.java
@@ -0,0 +1,13 @@
+package br.purpleapi.purple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurpleapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurpleapiApplication.class, args);
+	}
+
+}
---
+++ purpleapi/src/test/java/br/purpleapi/purple/PurpleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.purpleapi.purple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurpleapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
