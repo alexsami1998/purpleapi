0b16c3080f9e3e295fb69a4c5cd0b49d84615f3e
alexsami1998 <alexandre-enter86@live.com>
2023-10-16 15:31:20 -0300

    Repository left.

---
+++ purpleapi/src/main/java/br/purple/purpleapi/repositories/OperadorRepository.java
@@ -0,0 +1,9 @@
+package br.purple.purpleapi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.purple.purpleapi.domain.Operador;
+
+public interface OperadorRepository extends JpaRepository<Operador, Integer> {
+
+}
