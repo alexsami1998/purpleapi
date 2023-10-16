782637554653c2e0408b4a6e36b78783e85b923f
alexsami1998 <alexandre-enter86@live.com>
2023-10-16 15:53:03 -0300

    update jwt and packages.

--- purpleapi/src/main/java/br/purple/purpleapi/security/JWTAuthenticationFilter.java
+++ purpleapi/src/main/java/br/purple/purpleapi/security/JWTAuthenticationFilter.java
@@ -1,75 +1,74 @@
 package br.purple.purpleapi.security;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
-import javax.naming.AuthenticationException;
+import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import br.purple.purpleapi.domain.dtos.CredenciaisDTO;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-	
+
 	private AuthenticationManager authenticationManager;
 	private JWTUtil jwtUtil;
-	
+
 	public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
 		super();
 		this.authenticationManager = authenticationManager;
 		this.jwtUtil = jwtUtil;
 	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		try {
+			CredenciaisDTO creds = new ObjectMapper().readValue(request.getInputStream(), CredenciaisDTO.class);
+			UsernamePasswordAuthenticationToken authenticationToken = 
+					new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getSenha(), new ArrayList<>());
+			Authentication authentication = authenticationManager.authenticate(authenticationToken);
+			return authentication;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 	
 	@Override
-	public Authentication attempAuthentication(HttpServletRequest request, HttpServletResponse response) {
-		throws AuthenticationException {
-			try {
-				CredenciaisDTO creds = new ObjectMapper().readValue(request.getInputStream(), CredenciaisDTO.class);
-				UsernamePasswordAuthenticationToken authenticationToken = 
-						new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getSenha(), new ArrayList<>());
-				Authentication authentication = authenticationManager.authenticate(authenticationToken);
-				return authentication;
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		}
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
 		
-		@Override
-	    protected void successfulAuthentication(HttpServletRequest req,
-	                                            HttpServletResponse res,
-	                                            FilterChain chain,
-	                                            Authentication auth) throws IOException, ServletException {
+		String username = ((UserSS) authResult.getPrincipal()).getUsername();
+		String token = jwtUtil.generateToken(username);
+		response.setHeader("access-control-expose-headers", "Authorization");
+		response.setHeader("Authorization", "Bearer " + token);
+	}
+	
+	@Override
+	protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException failed) throws IOException, ServletException {
 		
-			String username = ((UserSS) auth.getPrincipal()).getUsername();
-			String token = jwtUtil.generateToken(username);
-			response.setHeader("Access-Control-Allow-Origin", "*");
-	        	response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-	        	response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, enctype, Location");
-	        	response.setHeader("Authorization", "Bearer " + token);
-		}
-		
-		@Override
-		protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response,
-				AuthenticationException failed) throws IOException, ServletException {
-			
-			response.setStatus(401);
-			response.setContentType("application/json");
-			response.getWriter().append(json());
-		}
+		response.setStatus(401);
+		response.setContentType("application/json");
+		response.getWriter().append(json());
+	}
 
-		private CharSequence json() {
-			long date = new Date().getTime();
-			return "{"
-					+ "\"timestamp\": " + date + ", " 
-					+ "\"status\": 401, "
-					+ "\"error\": \"Não autorizado\", "
-					+ "\"message\": \"Email ou senha inválidos\", "
-					+ "\"path\": \"/login\"}";
-		}
-}
+	private CharSequence json() {
+		long date = new Date().getTime();
+		return "{"
+				+ "\"timestamp\": " + date + ", " 
+				+ "\"status\": 401, "
+				+ "\"error\": \"Não autorizado\", "
+				+ "\"message\": \"Email ou senha inválidos\", "
+				+ "\"path\": \"/login\"}";
+	}
 }
