d6a34a219427b35fdd787b3e7c677acf3447cc63
alexsami1998 <alexandre-enter86@live.com>
2023-10-16 11:30:52 -0300

    Implements class of domain client.

--- purpleapi/src/main/java/br/purple/purpleapi/domain/Cliente.java
+++ purpleapi/src/main/java/br/purple/purpleapi/domain/Cliente.java
@@ -2,31 +2,51 @@ package br.purple.purpleapi.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import br.purple.purpleapi.domain.dtos.ClienteDTO;
 import br.purple.purpleapi.domain.enums.Perfil;
 
 @Entity
 public class Cliente extends Pessoa {
-	
+
 	private static final long serialVersionUID = 1L;
 	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
-	private List<Produto> produto = new ArrayList<>();
-	
-	public Cliente () {
+	private List<Produto> produtos = new ArrayList<>();
+
+	public Cliente() {
 		super();
 		addPerfil(Perfil.CLIENTE);
 	}
-	
+
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.CLIENTE);
 	}
-	
+
+	public Cliente(ClienteDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 }
