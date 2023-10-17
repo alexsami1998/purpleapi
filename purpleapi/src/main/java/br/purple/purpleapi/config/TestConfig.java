293c2794d647c6cacea34612d3394f94d1c2c116
alexsami1998 <alexandre-enter86@live.com>
2023-10-17 14:50:38 -0300

    Implements base tests and development.

--- purpleapi/src/main/java/br/purple/purpleapi/config/TestConfig.java
+++ purpleapi/src/main/java/br/purple/purpleapi/config/TestConfig.java
@@ -1,7 +1,6 @@
 package br.purple.purpleapi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -9,20 +8,14 @@ import org.springframework.context.annotation.Profile;
 import br.purple.purpleapi.service.DBService;
 
 @Configuration
-@Profile("dev")
-public class DevConfig {
+@Profile("test")
+public class TestConfig {
 
 	@Autowired
 	private DBService dbService;
 	
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String value;
-	
 	@Bean
-	public boolean instanciaDB() {
-		if(value.equals("create")) {
-			this.dbService.instanciaDB();
-		}
-		return false;
+	public void instanciaDB() {
+		this.dbService.instanciaDB();
 	}
 }
